52ebef8164b70cd56003675f289d7a5bd148fcba
linjiayi5 <linjiayi5@hikvision.com.cn>
2023-04-04 17:06:05 +0800

    feat: bean异常类

---
+++ src/main/java/com/lin/springframework/beans/BeansException.java
@@ -0,0 +1,16 @@
+package com.lin.springframework.beans;
+
+/**
+ * @author linjiayi5
+ */
+public class BeansException extends RuntimeException {
+
+    public BeansException(String msg) {
+        super(msg);
+    }
+
+    public BeansException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+}
