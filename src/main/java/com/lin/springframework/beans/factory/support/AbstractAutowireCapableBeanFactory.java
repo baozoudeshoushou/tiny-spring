72ec61ae230b0c3aba975046b4ea061e9ef68a9c
linjiayi5 <linjiayi5@hikvision.com.cn>
2023-04-06 16:20:40 +0800

    feat: 创建 bean 实例时选举构造器，并使用 InstantiationStrategy

--- src/main/java/com/lin/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/com/lin/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -3,6 +3,8 @@ package com.lin.springframework.beans.factory.support;
 import com.lin.springframework.beans.BeansException;
 import com.lin.springframework.beans.factory.config.BeanDefinition;
 
+import java.lang.reflect.Constructor;
+
 /**
  * Abstract bean factory superclass that implements default bean creation
  * @Author linjiayi5
@@ -10,18 +12,51 @@ import com.lin.springframework.beans.factory.config.BeanDefinition;
  */
 public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory {
 
+    private InstantiationStrategy instantiationStrategy = new SimpleInstantiationStrategy();
+
     @Override
-    protected Object createBean(String beanName, BeanDefinition beanDefinition) throws BeansException {
+    protected Object createBean(String beanName, BeanDefinition beanDefinition, Object[] args) throws BeansException {
         Object bean;
         Class beanClass = beanDefinition.getBeanClass();
         try {
-            // TODO 这里应该选举构造器、必要时注入 bean，
-            bean = beanClass.newInstance();
-        } catch (InstantiationException | IllegalAccessException e) {
+            bean = createBeanInstance(beanName, beanDefinition, args);
+        } catch (Exception e) {
             throw new BeansException("Instantiation of bean failed", e);
         }
         addSingleton(beanName, bean);
         return bean;
+    }
+
+    /**
+     * Create a new instance for the specified bean, using an appropriate instantiation strategy:
+     * factory method, constructor autowiring, or simple instantiation.
+     * @param beanName beanName the name of the bean
+     * @param beanDefinition the bean definition for the bean
+     * @param args explicit arguments to use for constructor or factory method invocation
+     * @return
+     */
+    protected Object createBeanInstance(String beanName, BeanDefinition beanDefinition, Object[] args) {
+        Constructor<?> constructorToUse = null;
+        Class<?> beanClass = beanDefinition.getBeanClass();
+        if (args != null) {
+            Constructor<?>[] declaredConstructors = beanClass.getDeclaredConstructors();
+            for (Constructor<?> ctor : declaredConstructors) {
+                if (ctor.getParameterCount() == args.length) {
+                    constructorToUse = ctor;
+                    break;
+                }
+            }
+
+        }
+        return getInstantiationStrategy().instantiate(beanDefinition, beanName,constructorToUse, args);
+    }
+
+    public InstantiationStrategy getInstantiationStrategy() {
+        return instantiationStrategy;
+    }
+
+    public void setInstantiationStrategy(InstantiationStrategy instantiationStrategy) {
+        this.instantiationStrategy = instantiationStrategy;
     }
 
 }
