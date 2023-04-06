6162e03ce6ed895615769504af5948dceecc6dc8
linjiayi5 <linjiayi5@hikvision.com.cn>
2023-04-06 14:55:24 +0800

    feat: bean 定义

--- src/main/java/com/lin/springframework/beans/factory/config/BeanDefinition.java
+++ src/main/java/com/lin/springframework/beans/factory/config/BeanDefinition.java
@@ -1,6 +1,9 @@
 package com.lin.springframework.beans.factory.config;
 
 /**
+ * A BeanDefinition describes a bean instance, which has property values,
+ * constructor argument values, and further information supplied by
+ * concrete implementations.
  * @Author linjiayi5
  * @Date 2023/4/4 16:14:01
  */
@@ -10,6 +13,10 @@ public class BeanDefinition {
 
     public BeanDefinition(Class beanClass) {
         this.beanClass = beanClass;
+    }
+
+    public Class getBeanClass() {
+        return beanClass;
     }
 
 }
