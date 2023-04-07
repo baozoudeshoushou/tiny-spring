3c4604992cbb9128c8704f4bf6862d3108eb64f2
linjiayi5 <linjiayi5@hikvision.com.cn>
2023-04-07 20:44:59 +0800

    feat: test xml

--- src/test/java/com/lin/springframework/ApiTest.java
+++ src/test/java/com/lin/springframework/ApiTest.java
@@ -1,5 +1,6 @@
 package com.lin.springframework;
 
+import cn.hutool.core.io.IoUtil;
 import com.lin.springframework.bean.UserDao;
 import com.lin.springframework.bean.UserService;
 import com.lin.springframework.beans.PropertyValue;
@@ -8,7 +9,14 @@ import com.lin.springframework.beans.factory.config.BeanDefinition;
 import com.lin.springframework.beans.factory.config.BeanReference;
 import com.lin.springframework.beans.factory.support.DefaultListableBeanFactory;
 import com.lin.springframework.beans.factory.support.SimpleInstantiationStrategy;
+import com.lin.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import com.lin.springframework.core.io.DefaultResourceLoader;
+import com.lin.springframework.core.io.Resource;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @Author linjiayi5
@@ -32,5 +40,42 @@ public class ApiTest {
         userService.queryUserInfo();
     }
 
+    private DefaultResourceLoader resourceLoader;
+
+    @Before
+    public void init() {
+        resourceLoader = new DefaultResourceLoader();
+    }
+    @Test
+    public void test_classpath() throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+    @Test
+    public void test_file() throws IOException {
+        Resource resource = resourceLoader.getResource("src/test/resources/important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+    @Test
+    public void test_url() throws IOException {
+        Resource resource = resourceLoader.getResource("https://github.com/fuzhengwei/small-spring/blob/main/important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+
+    @Test
+    public void test_xml() {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(beanFactory, resourceLoader);
+        xmlBeanDefinitionReader.loadBeanDefinitions(resourceLoader.getResource("classpath:spring.xml"));
+
+        UserService userService = beanFactory.getBean("userService", UserService.class);
+        userService.queryUserInfo();
+    }
 
 }
