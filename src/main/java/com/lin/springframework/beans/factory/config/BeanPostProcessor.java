d1a1b4f43b8e9bb3617ec7e54570e28b292edfff
linjiayi5 <linjiayi5@hikvision.com.cn>
2023-04-07 21:21:34 +0800

    feat: BeanPostProcessor

---
+++ src/main/java/com/lin/springframework/beans/factory/config/BeanPostProcessor.java
@@ -0,0 +1,37 @@
+package com.lin.springframework.beans.factory.config;
+
+import com.lin.springframework.beans.BeansException;
+
+/**
+ * Factory hook that allows for custom modification of new bean instances &mdash;
+ * for example, checking for marker interfaces or wrapping beans with proxies.
+ *
+ * 用于修改新实例化 Bean 对象的扩展点
+ *
+ * @Author linjiayi5
+ * @Date 2023/4/7 21:10:24
+ * @see BeanPostProcessor
+ */
+public interface BeanPostProcessor {
+
+    /**
+     * 在 Bean 对象执行初始化方法之前，执行此方法
+     *
+     * @param bean
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
+    Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException;
+
+    /**
+     * 在 Bean 对象执行初始化方法之后，执行此方法
+     *
+     * @param bean
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
+    Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException;
+
+}
