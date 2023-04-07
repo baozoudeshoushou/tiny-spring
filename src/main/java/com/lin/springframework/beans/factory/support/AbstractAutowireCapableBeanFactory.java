a9806359451d230ad8b4c74557de06b33d8ed621
linjiayi5 <linjiayi5@hikvision.com.cn>
2023-04-07 11:10:51 +0800

    feat: bean 屬性填充

--- src/main/java/com/lin/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/com/lin/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -1,7 +1,11 @@
 package com.lin.springframework.beans.factory.support;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.lin.springframework.beans.BeansException;
+import com.lin.springframework.beans.PropertyValue;
+import com.lin.springframework.beans.PropertyValues;
 import com.lin.springframework.beans.factory.config.BeanDefinition;
+import com.lin.springframework.beans.factory.config.BeanReference;
 
 import java.lang.reflect.Constructor;
 
@@ -20,6 +24,7 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
         Class beanClass = beanDefinition.getBeanClass();
         try {
             bean = createBeanInstance(beanName, beanDefinition, args);
+            applyPropertyValues(beanName, bean, beanDefinition);
         } catch (Exception e) {
             throw new BeansException("Instantiation of bean failed", e);
         }
@@ -51,7 +56,25 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
         return getInstantiationStrategy().instantiate(beanDefinition, beanName,constructorToUse, args);
     }
 
-    public InstantiationStrategy getInstantiationStrategy() {
+    /**
+     * pulate the bean instance with the property values from the bean definition.
+     * @param beanName the name of the bean
+     * @param bean the bean
+     * @param beanDefinition the bean definition for the bean
+     */
+    protected void applyPropertyValues(String beanName, Object bean, BeanDefinition beanDefinition) {
+        PropertyValues propertyValues = beanDefinition.getPropertyValues();
+        for (PropertyValue propertyValue : propertyValues.getPropertyValues()) {
+            String name = propertyValue.getName();
+            Object value = propertyValue.getValue();
+            if (value instanceof BeanReference beanReference) {
+                value = getBean(beanReference.getBeanName());
+            }
+            BeanUtil.setFieldValue(bean, name, value);
+        }
+    }
+
+        public InstantiationStrategy getInstantiationStrategy() {
         return instantiationStrategy;
     }
 
