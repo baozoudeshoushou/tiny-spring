b05d96e5f42223c753d35cb75886e257696b5de0
linjiayi <linjiayi030@163.com>
2023-04-09 23:09:30 +0800

    feat: refresh

--- src/main/java/com/lin/springframework/context/ConfigurableApplicationContext.java
+++ src/main/java/com/lin/springframework/context/ConfigurableApplicationContext.java
@@ -1,8 +1,25 @@
 package com.lin.springframework.context;
 
+import com.lin.springframework.beans.BeansException;
+
 /**
+ * SPI interface to be implemented by most if not all application contexts.
+ * Provides facilities to configure an application context in addition
+ * to the application context client methods in the
+ *
  * @Author linjiayi5
  * @Date 2023/4/7 21:14:18
  */
-public interface ConfigurableApplicationContext {
+public interface ConfigurableApplicationContext extends ApplicationContext {
+
+    /**
+     * 核心方法，刷新容器
+     * Load or refresh the persistent representation of the configuration, which
+     * might be from Java-based configuration, an XML file, a properties file, a
+     * relational database schema, or some other format.
+     *
+     * @throws BeansException
+     */
+    void refresh() throws BeansException;
+
 }
--- src/main/java/com/lin/springframework/context/support/AbstractApplicationContext.java
+++ src/main/java/com/lin/springframework/context/support/AbstractApplicationContext.java
@@ -1,8 +1,45 @@
 package com.lin.springframework.context.support;
 
+import com.lin.springframework.beans.BeansException;
+import com.lin.springframework.beans.factory.ConfigurableListableBeanFactory;
+import com.lin.springframework.context.ConfigurableApplicationContext;
+import com.lin.springframework.core.io.DefaultResourceLoader;
+
 /**
  * @Author linjiayi5
  * @Date 2023/4/7 21:12:51
  */
-public abstract class AbstractApplicationContext {
+public abstract class AbstractApplicationContext extends DefaultResourceLoader implements ConfigurableApplicationContext {
+
+    @Override
+    public void refresh() throws BeansException {
+        // 1. 创建 Beanfactory，并加载 BeanDefinition
+        refreshBeanFactory();
+
+        // 2. 获取 BeanFactory
+        ConfigurableListableBeanFactory beanFactory = getBeanFactory();
+
+        // 3. 在 Bean 实例化之前执行 BeanFactoryPostProcessor
+        invokeBeanFactoryPostProcessors(beanFactory);
+
+        // 4. BeanPostProcessor 需要提前于其他 Bean 对象实例化之前执行注册操作
+        registerBeanPostProcessors(beanFactory);
+
+        // 5. 提前实例化单例 Bean 对象
+        beanFactory.preInstantiateSingletons();
+
+    }
+
+    protected abstract void refreshBeanFactory() throws BeansException;
+
+    protected abstract ConfigurableListableBeanFactory getBeanFactory();
+
+    protected void invokeBeanFactoryPostProcessors(ConfigurableListableBeanFactory beanFactory) {
+
+    }
+
+    private void registerBeanPostProcessors(ConfigurableListableBeanFactory beanFactory) {
+
+    }
+
 }
