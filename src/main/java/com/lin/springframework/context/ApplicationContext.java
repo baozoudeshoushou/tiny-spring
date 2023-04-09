74dce29d0da717b7b49f9a66e7af6f2e817294c0
linjiayi <linjiayi030@163.com>
2023-04-09 23:09:21 +0800

    feat: ApplicationContext

--- src/main/java/com/lin/springframework/context/ApplicationContext.java
+++ src/main/java/com/lin/springframework/context/ApplicationContext.java
@@ -1,8 +1,16 @@
 package com.lin.springframework.context;
 
+import com.lin.springframework.beans.factory.ListableBeanFactory;
+
 /**
+ * Central interface to provide configuration for an application.
+ * This is read-only while the application is running, but may be
+ * reloaded if the implementation supports this.
+ *
  * @Author linjiayi5
  * @Date 2023/4/7 21:12:42
  */
-public interface ApplicationContext {
+public interface ApplicationContext extends ListableBeanFactory {
+
+
 }
