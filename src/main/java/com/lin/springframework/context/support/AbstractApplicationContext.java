01c34483c59f24d53fc904fdff9eb6f0ede52d49
linjiayi5 <linjiayi5@hikvision.com.cn>
2023-04-10 11:22:51 +0800

    feat: refresh

--- src/main/java/com/lin/springframework/context/support/AbstractApplicationContext.java
+++ src/main/java/com/lin/springframework/context/support/AbstractApplicationContext.java
@@ -2,8 +2,12 @@ package com.lin.springframework.context.support;
 
 import com.lin.springframework.beans.BeansException;
 import com.lin.springframework.beans.factory.ConfigurableListableBeanFactory;
+import com.lin.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import com.lin.springframework.beans.factory.config.BeanPostProcessor;
 import com.lin.springframework.context.ConfigurableApplicationContext;
 import com.lin.springframework.core.io.DefaultResourceLoader;
+
+import java.util.Map;
 
 /**
  * @Author linjiayi5
@@ -34,12 +38,28 @@ public abstract class AbstractApplicationContext extends DefaultResourceLoader i
 
     protected abstract ConfigurableListableBeanFactory getBeanFactory();
 
+    /**
+     * Instantiate and invoke all registered BeanFactoryPostProcessor beans,
+     * respecting explicit order if given.
+     * <p>Must be called before singleton instantiation.
+     */
     protected void invokeBeanFactoryPostProcessors(ConfigurableListableBeanFactory beanFactory) {
-
+        Map<String, BeanFactoryPostProcessor> beanFactoryPostProcessorMap = beanFactory.getBeansOfType(BeanFactoryPostProcessor.class);
+        for (BeanFactoryPostProcessor beanFactoryPostProcessor : beanFactoryPostProcessorMap.values()) {
+            beanFactoryPostProcessor.postProcessBeanFactory(beanFactory);
+        }
     }
 
+    /**
+     * Instantiate and register all BeanPostProcessor beans,
+     * respecting explicit order if given.
+     * <p>Must be called before any instantiation of application beans.
+     */
     private void registerBeanPostProcessors(ConfigurableListableBeanFactory beanFactory) {
-
+        Map<String, BeanPostProcessor> beanPostProcessorMap = beanFactory.getBeansOfType(BeanPostProcessor.class);
+        for (BeanPostProcessor beanPostProcessor : beanPostProcessorMap.values()) {
+            beanFactory.addBeanPostProcessor(beanPostProcessor);
+        }
     }
 
 }
