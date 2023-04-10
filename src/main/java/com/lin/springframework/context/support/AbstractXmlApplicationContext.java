8ad2ea3ef685f19cf00683af36f3bb9f73824c2d
linjiayi5 <linjiayi5@hikvision.com.cn>
2023-04-10 16:30:37 +0800

    feat: XmlApplicationContext

--- src/main/java/com/lin/springframework/context/support/AbstractXmlApplicationContext.java
+++ src/main/java/com/lin/springframework/context/support/AbstractXmlApplicationContext.java
@@ -1,8 +1,29 @@
 package com.lin.springframework.context.support;
 
+import com.lin.springframework.beans.factory.support.DefaultListableBeanFactory;
+import com.lin.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
 /**
  * @Author linjiayi5
  * @Date 2023/4/7 21:13:44
  */
-public abstract class AbstractXmlApplicationContext {
+public abstract class AbstractXmlApplicationContext extends AbstractRefreshableApplicationContext {
+
+    @Override
+    protected void loadBeanDefinitions(DefaultListableBeanFactory beanFactory) {
+        // Create a new XmlBeanDefinitionReader for the given BeanFactory.
+        XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+        String[] configLocations = getConfigLocations();
+        if (configLocations != null) {
+            beanDefinitionReader.loadBeanDefinitions(configLocations);
+        }
+    }
+
+    /**
+     * Return an array of resource locations, referring to the XML bean definition
+     * files that this context should be built with.
+     * @return an array of resource locations, or {@code null} if none
+     */
+    protected abstract String[] getConfigLocations();
+
 }
--- src/main/java/com/lin/springframework/context/support/ClassPathXmlApplicationContext.java
+++ src/main/java/com/lin/springframework/context/support/ClassPathXmlApplicationContext.java
@@ -4,5 +4,27 @@ package com.lin.springframework.context.support;
  * @Author linjiayi5
  * @Date 2023/4/7 21:13:52
  */
-public class ClassPathXmlApplicationContext {
+public class ClassPathXmlApplicationContext extends AbstractXmlApplicationContext {
+
+    private String[] configLocations;
+
+    public ClassPathXmlApplicationContext() {
+
+    }
+
+    public ClassPathXmlApplicationContext(String configLocation) {
+        this(new String[] {configLocation});
+    }
+
+    public ClassPathXmlApplicationContext(String[] configLocations) {
+        this.configLocations = configLocations;
+        refresh();
+    }
+
+
+    @Override
+    protected String[] getConfigLocations() {
+        return configLocations;
+    }
+
 }
