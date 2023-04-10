bd859c231ab3237dc37a001f0facb2176cd0503e
linjiayi5 <linjiayi5@hikvision.com.cn>
2023-04-10 17:12:03 +0800

    feat: apply BeanPostProcessors

--- src/main/java/com/lin/springframework/beans/factory/config/AutowireCapableBeanFactory.java
+++ src/main/java/com/lin/springframework/beans/factory/config/AutowireCapableBeanFactory.java
@@ -1,5 +1,6 @@
 package com.lin.springframework.beans.factory.config;
 
+import com.lin.springframework.beans.BeansException;
 import com.lin.springframework.beans.factory.BeanFactory;
 
 /**
@@ -7,5 +8,29 @@ import com.lin.springframework.beans.factory.BeanFactory;
  * @Date 2023/4/7 20:51:29
  */
 public interface AutowireCapableBeanFactory extends BeanFactory {
+
+    /**
+     * Apply {@link BeanPostProcessor BeanPostProcessors} to the given existing bean
+     * instance, invoking their {@code postProcessBeforeInitialization} methods.
+     * The returned bean instance may be a wrapper around the original.
+     * @param existingBean the existing bean instance
+     * @param beanName the name of the bean, to be passed to it if necessary
+     * @return the bean instance to use, either the original or a wrapped one
+     * @throws BeansException if any post-processing failed
+     * @see BeanPostProcessor#postProcessBeforeInitialization
+     */
+    Object applyBeanPostProcessorsBeforeInitialization(Object existingBean, String beanName) throws BeansException;
+
+    /**
+     * Apply {@link BeanPostProcessor BeanPostProcessors} to the given existing bean
+     * instance, invoking their {@code postProcessAfterInitialization} methods.
+     * The returned bean instance may be a wrapper around the original.
+     * @param existingBean the existing bean instance
+     * @param beanName the name of the bean, to be passed to it if necessary
+     * @return the bean instance to use, either the original or a wrapped one
+     * @throws BeansException if any post-processing failed
+     * @see BeanPostProcessor#postProcessAfterInitialization
+     */
+    Object applyBeanPostProcessorsAfterInitialization(Object existingBean, String beanName) throws BeansException;
 
 }
--- src/main/java/com/lin/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/com/lin/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -4,7 +4,9 @@ import cn.hutool.core.bean.BeanUtil;
 import com.lin.springframework.beans.BeansException;
 import com.lin.springframework.beans.PropertyValue;
 import com.lin.springframework.beans.PropertyValues;
+import com.lin.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import com.lin.springframework.beans.factory.config.BeanDefinition;
+import com.lin.springframework.beans.factory.config.BeanPostProcessor;
 import com.lin.springframework.beans.factory.config.BeanReference;
 
 import java.lang.reflect.Constructor;
@@ -14,17 +16,19 @@ import java.lang.reflect.Constructor;
  * @Author linjiayi5
  * @Date 2023/4/4 17:09:33
  */
-public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory {
+public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory implements AutowireCapableBeanFactory {
 
     private InstantiationStrategy instantiationStrategy = new SimpleInstantiationStrategy();
 
     @Override
     protected Object createBean(String beanName, BeanDefinition beanDefinition, Object[] args) throws BeansException {
         Object bean;
-        Class beanClass = beanDefinition.getBeanClass();
         try {
             bean = createBeanInstance(beanName, beanDefinition, args);
+            // 给 Bean 填充属性
             applyPropertyValues(beanName, bean, beanDefinition);
+            // 执行 Bean 的初始化方法和 BeanPostProcessor 的前置和后置处理方法
+            Object o = initializeBean(beanName, bean, beanDefinition);
         } catch (Exception e) {
             throw new BeansException("Instantiation of bean failed", e);
         }
@@ -74,12 +78,63 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
         }
     }
 
-        public InstantiationStrategy getInstantiationStrategy() {
+    public InstantiationStrategy getInstantiationStrategy() {
         return instantiationStrategy;
     }
 
     public void setInstantiationStrategy(InstantiationStrategy instantiationStrategy) {
         this.instantiationStrategy = instantiationStrategy;
+    }
+
+    /**
+     * Initialize the given bean instance, applying factory callbacks
+     * as well as init methods and bean post processors.
+     * @param beanName the bean name in the factory (for debugging purposes)
+     * @param bean the new bean instance we may need to initialize
+     * @param beanDefinition the bean definition that the bean was created with
+     * @return the initialized bean instance (potentially wrapped)
+     */
+    private Object initializeBean(String beanName, Object bean, BeanDefinition beanDefinition) {
+        Object wrappedBean = bean;
+
+        wrappedBean = applyBeanPostProcessorsBeforeInitialization(wrappedBean, beanName);
+
+        invokeInitMethods(beanName, wrappedBean, beanDefinition);
+
+        wrappedBean = applyBeanPostProcessorsAfterInitialization(wrappedBean, beanName);
+
+        return wrappedBean;
+    }
+
+
+    private void invokeInitMethods(String beanName, Object wrappedBean, BeanDefinition beanDefinition) {
+        // TODO
+    }
+
+    @Override
+    public Object applyBeanPostProcessorsBeforeInitialization(Object existingBean, String beanName) throws BeansException {
+        Object result = existingBean;
+        for (BeanPostProcessor beanPostProcessor : getBeanPostProcessors()) {
+            Object current = beanPostProcessor.postProcessBeforeInitialization(result, beanName);
+            if (current == null) {
+                return result;
+            }
+            result = current;
+        }
+        return result;
+    }
+
+    @Override
+    public Object applyBeanPostProcessorsAfterInitialization(Object existingBean, String beanName) throws BeansException {
+        Object result = existingBean;
+        for (BeanPostProcessor beanPostProcessor : getBeanPostProcessors()) {
+            Object current = beanPostProcessor.postProcessAfterInitialization(result, beanName);
+            if (current == null) {
+                return result;
+            }
+            result = current;
+        }
+        return result;
     }
 
 }
