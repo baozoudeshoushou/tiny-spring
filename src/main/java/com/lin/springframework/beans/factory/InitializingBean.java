81b39cb009949d3c8d9a602734bfb75782e2b11b
linjiayi5 <linjiayi5@hikvision.com.cn>
2023-04-10 20:01:40 +0800

    feat: InitializingBean

---
+++ src/main/java/com/lin/springframework/beans/factory/InitializingBean.java
@@ -0,0 +1,24 @@
+package com.lin.springframework.beans.factory;
+
+/**
+ * Interface to be implemented by beans that need to react once all their properties
+ * have been set by a {@link BeanFactory}: e.g. to perform custom initialization,
+ * or merely to check that all mandatory properties have been set.
+ *
+ * @Author linjiayi5
+ * @Date 2023/4/10 18:45:40
+ * @see DisposableBean
+ */
+public interface InitializingBean {
+
+    /**
+     * Invoked by the containing {@code BeanFactory} after it has set all bean properties
+     * and satisfied {@link BeanFactoryAware}, {@code ApplicationContextAware} etc.
+     * <p>This method allows the bean instance to perform validation of its overall
+     * configuration and final initialization when all bean properties have been set.
+     * @throws Exception in the event of misconfiguration (such as failure to set an
+     * essential property) or if initialization fails for any other reason
+     */
+    void afterPropertiesSet() throws Exception;
+
+}
