e427d46170187b58a89cbaaaa07dda427c3cb883
linjiayi5 <linjiayi5@hikvision.com.cn>
2023-04-10 16:29:54 +0800

    feat: create and refresh BeanFactory

--- src/main/java/com/lin/springframework/context/support/AbstractRefreshableApplicationContext.java
+++ src/main/java/com/lin/springframework/context/support/AbstractRefreshableApplicationContext.java
@@ -1,8 +1,43 @@
 package com.lin.springframework.context.support;
 
+import com.lin.springframework.beans.BeansException;
+import com.lin.springframework.beans.factory.ConfigurableListableBeanFactory;
+import com.lin.springframework.beans.factory.support.DefaultListableBeanFactory;
+
 /**
  * @Author linjiayi5
  * @Date 2023/4/7 21:13:28
  */
-public abstract class AbstractRefreshableApplicationContext {
+public abstract class AbstractRefreshableApplicationContext extends AbstractApplicationContext {
+
+    private DefaultListableBeanFactory beanFactory;
+
+    @Override
+    protected void refreshBeanFactory() throws BeansException {
+        DefaultListableBeanFactory beanFactory = createBeanFactory();
+        loadBeanDefinitions(beanFactory);
+        this.beanFactory = beanFactory;
+    }
+
+    private DefaultListableBeanFactory createBeanFactory() {
+        return new DefaultListableBeanFactory();
+    }
+
+    @Override
+    protected ConfigurableListableBeanFactory getBeanFactory() {
+        DefaultListableBeanFactory beanFactory = this.beanFactory;
+        if (beanFactory == null) {
+            throw new IllegalStateException("BeanFactory not initialized or already closed - " +
+                    "call 'refresh' before accessing beans via the ApplicationContext");
+        }
+        return beanFactory;
+    }
+
+    /**
+     * Load bean definitions into the given bean factory, typically through
+     * delegating to one or more bean definition readers.
+     * @param beanFactory the bean factory to load bean definitions into
+     */
+    protected abstract void loadBeanDefinitions(DefaultListableBeanFactory beanFactory);
+
 }
