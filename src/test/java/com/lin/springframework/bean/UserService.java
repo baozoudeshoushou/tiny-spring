32cda7851174b0b44d750ffee7b07cd9b2c75613
linjiayi5 <linjiayi5@hikvision.com.cn>
2023-04-11 11:20:22 +0800

    feat: test

--- src/test/java/com/lin/springframework/bean/UserService.java
+++ src/test/java/com/lin/springframework/bean/UserService.java
@@ -1,9 +1,15 @@
 package com.lin.springframework.bean;
 
-import com.lin.springframework.beans.factory.DisposableBean;
-import com.lin.springframework.beans.factory.InitializingBean;
+import com.lin.springframework.beans.BeansException;
+import com.lin.springframework.beans.factory.*;
+import com.lin.springframework.context.ApplicationContext;
+import com.lin.springframework.context.ApplicationContextAware;
 
-public class UserService implements InitializingBean, DisposableBean {
+public class UserService implements InitializingBean, DisposableBean, BeanNameAware, BeanClassLoaderAware, ApplicationContextAware, BeanFactoryAware {
+
+    private ApplicationContext applicationContext;
+
+    private BeanFactory beanFactory;
 
     private String uId;
 
@@ -59,5 +65,26 @@ public class UserService implements InitializingBean, DisposableBean {
         this.location = location;
     }
 
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        System.out.println("ClassLoader：" + classLoader);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("beanFactory：" + beanFactory);
+        this.beanFactory = beanFactory;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Bean Name is：" + name);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("applicationContext：" + applicationContext);
+        this.applicationContext = applicationContext;
+    }
 }
 
