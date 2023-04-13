ade330d8590c410821c6e27f45bf09e290aed3c7
linjiayi5 <linjiayi5@hikvision.com.cn>
2023-04-13 11:38:17 +0800

    feat: extends EventListener

--- src/main/java/com/lin/springframework/context/ApplicationListener.java
+++ src/main/java/com/lin/springframework/context/ApplicationListener.java
@@ -1,12 +1,19 @@
 package com.lin.springframework.context;
 
+import java.util.EventListener;
+
 /**
  * Interface to be implemented by application event listeners.
  *
  * @Author linjiayi5
  * @Date 2023/4/11 15:58:14
  */
-public interface ApplicationListener<T> {
+public interface ApplicationListener<E extends ApplicationEvent> extends EventListener {
 
+    /**
+     * Handle an application event.
+     * @param event the event to respond to
+     */
+    void onApplicationEvent(E event);
 
 }
