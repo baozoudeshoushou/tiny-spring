bbbca5bae538784ec3910b40869774287b66227f
linjiayi5 <linjiayi5@hikvision.com.cn>
2023-04-18 16:38:54 +0800

    feat: embeddedValueResolvers

--- src/main/java/com/lin/springframework/beans/factory/support/AbstractBeanFactory.java
+++ src/main/java/com/lin/springframework/beans/factory/support/AbstractBeanFactory.java
@@ -9,9 +9,11 @@ import com.lin.springframework.beans.factory.config.BeanDefinition;
 import com.lin.springframework.beans.factory.config.BeanPostProcessor;
 import com.lin.springframework.beans.factory.config.ConfigurableBeanFactory;
 import com.lin.springframework.utils.ClassUtils;
+import com.lin.springframework.utils.StringValueResolver;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * @Author linjiayi5
@@ -23,6 +25,9 @@ public abstract class AbstractBeanFactory extends FactoryBeanRegistrySupport imp
 
     /** BeanPostProcessors to apply. */
     private final List<BeanPostProcessor> beanPostProcessors = new ArrayList<>();
+
+    /** String resolvers to apply e.g. to annotation attribute values. */
+    private final List<StringValueResolver> embeddedValueResolvers = new CopyOnWriteArrayList<>();
 
     //---------------------------------------------------------------------
     // Implementation of BeanFactory interface
@@ -144,6 +149,26 @@ public abstract class AbstractBeanFactory extends FactoryBeanRegistrySupport imp
     @Override
     public ClassLoader getBeanClassLoader() {
         return this.beanClassLoader;
+    }
+
+    @Override
+    public void addEmbeddedValueResolver(StringValueResolver valueResolver) {
+        Assert.notNull(valueResolver, "StringValueResolver must not be null");
+        this.embeddedValueResolvers.add(valueResolver);
+    }
+
+    @Override
+    public boolean hasEmbeddedValueResolver() {
+        return !this.embeddedValueResolvers.isEmpty();
+    }
+
+    @Override
+    public String resolveEmbeddedValue(String value) {
+        String result = value;
+        for (StringValueResolver resolver : this.embeddedValueResolvers) {
+            result = resolver.resolveStringValue(result);
+        }
+        return result;
     }
 
 }
